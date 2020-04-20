98388a4565a6fc532cea3e72170996adf699a6ed
briceln <brice.lang-nguyen@epitech.eu>
2020-04-20 13:40:34 +0200

    :sparkles: Add wifi connection

---
+++ src/src.ino
@@ -0,0 +1,60 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+
+#include "arduino_secrets.h"
+
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int status = WL_IDLE_STATUS;     // the WiFi radio's status
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 5 seconds for connection:
+    delay(5000);
+  }
+
+  Serial.println("You're connected to the network");
+}
+
+void loop() { 
+  Serial.print("Pinging ");
+  Serial.print(hostName);
+  Serial.print(": ");
+
+  pingResult = WiFi.ping(hostName);
+
+  if (pingResult >= 0) {
+    Serial.print("SUCCESS! RTT = ");
+    Serial.print(pingResult);
+    Serial.println(" ms");
+  } else {
+    Serial.print("FAILED! Error code: ");
+    Serial.println(pingResult);
+  }
+
+  delay(5000);
+}
+
