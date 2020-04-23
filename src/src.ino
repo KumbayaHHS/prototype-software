db3460880d56607cea875579382f8f75864b2558
briceln <brice.lang-nguyen@epitech.eu>
2020-04-23 20:01:03 +0200

    :art: Improve comments style

--- src/src.ino
+++ src/src.ino
@@ -4,9 +4,9 @@
 
 #include "arduino_secrets.h"
 
-char ssid[] = SECRET_SSID;        // your network SSID (name)
+char ssid[] = SECRET_SSID;    // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
-int status = WL_IDLE_STATUS;     // the WiFi radio's status
+int status = WL_IDLE_STATUS;  // the WiFi radio's status
 
 //MQTT
 #define MQTT_BROKER       "maqiatto.com"
