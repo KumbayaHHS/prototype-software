68f7cec119c6d6b9affb9dae793baf096894577c
briceln <brice.lang-nguyen@epitech.eu>
2020-06-10 16:12:14 +0200

    :lock: Remove keys

--- src/secrets.h
+++ src/secrets.h
@@ -3,7 +3,7 @@
 #define DEVICE_ID "prototype"
 #define AZURE_HOSTNAME "kumbaya-hub.azure-devices.net"
 #define AZURE_USERNAME "kumbaya-hub.azure-devices.net/prototype/?api-version=2018-06-30"
-#define AZURE_PASSWORD "SharedAccessSignature sr=kumbaya-hub.azure-devices.net%2Fdevices%2Fprototype&sig=%2F0Sg8s8S2HgmCCDUMGi%2FU1h5Oi2gcVX6dhTQE5rQTg4%3D&se=1623123870"
+#define AZURE_PASSWORD ""
 #define AZURE_FEEDBACK_TOPIC "devices/prototype/messages/events/"
 #define AZURE_LED_TOPIC "devices/prototype/messages/devicebound/#"
 #define USER_ID "userTest"
